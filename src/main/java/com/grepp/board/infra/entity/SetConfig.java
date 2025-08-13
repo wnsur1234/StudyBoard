9755d8b85fbf1c114a3ff0ca3cbee6d2b04b545c
Jun <wnsur1234@gmail.com>
2025-08-13 16:47:24 +0900

    :sparkles: feat: Redirect root path to defualt page using setConfig id

---
+++ src/main/java/com/grepp/board/infra/entity/SetConfig.java
@@ -0,0 +1,23 @@
+package com.grepp.board.infra.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SetConfig {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(unique = true)
+    private Home defaultId;
+
+
+}
