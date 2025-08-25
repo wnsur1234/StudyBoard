04b940f1cd0b7762ac92688ca227e8ca2f72e50d
Jun <wnsur1234@gmail.com>
2025-08-25 18:17:13 +0900

    :sparkles: feat: Add feature to retrieve posts written by the current user

--- src/main/java/com/grepp/board/app/controller/web/RenderingWebController.java
+++ src/main/java/com/grepp/board/app/controller/web/RenderingWebController.java
@@ -1,8 +1,11 @@
 package com.grepp.board.app.controller.web;
 
 import com.grepp.board.app.controller.web.payload.LetterRequest;
+import com.grepp.board.app.controller.web.payload.LetterResponse;
 import com.grepp.board.app.controller.web.payload.RenderResponse;
+import com.grepp.board.app.model.dto.LetterDTO;
 import com.grepp.board.app.model.service.HomeLenderService;
+import com.grepp.board.app.model.service.LenderLetterService;
 import com.grepp.board.app.model.service.WriteLetterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -25,6 +30,7 @@ public class RenderingWebController {
      */
     private final HomeLenderService homeLenderService;
     private final WriteLetterService writeLetterService;
+    private final LenderLetterService lenderLetterService;
 
     // 화면을 나타낼 때 특정 데이터가 필요한것이므로
     // id 값을 받아서 home 엔티티에 id 값을 반환 하도록 변경해야함
@@ -33,15 +39,16 @@ public class RenderingWebController {
             @RequestParam(name="id",required = false) Long homeId, Model model)
     {
         RenderResponse home = RenderResponse.fromDTO(homeLenderService.getHomeById(homeId));
+        List<LetterResponse> letters = LetterResponse.fromDTOs(lenderLetterService.getAllLetter());
         model.addAttribute("home", home);
-        System.out.println(home);
+        model.addAttribute("letters", letters);
+        System.out.println(letters);
         return "board/home";
     }
     @GetMapping("/letter")
     public String letter(@RequestParam(name="id",required = false) Long homeId,Model model){
         RenderResponse home = RenderResponse.fromDTO(homeLenderService.getHomeById(homeId));
         model.addAttribute("home", home);
-        System.out.println(home);
         return "board/writer";
     }
     @PostMapping("/letter")
